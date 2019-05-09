0cc7ad8092062ea06e44c1d2ee0eace0ac61321a
jambrito <jambrito@gmail.com>
2019-05-09 18:21:15 -0300

    Initial commit

---
+++ R/Data_capitals.R
@@ -0,0 +1,7 @@
+#'Data_capitals
+#' @title File capitals
+#' @description Data Frame with 48 capitals of the US - TSP
+#' @format Data frame with 48 rows: (File capitals)
+#' @references 1. http://elib.zib.de/pub/mp-testdata/tsp/tsplib/tsp/index.html
+"Data_capitals"
+
